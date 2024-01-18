a5f09c7f9c7b8256e6421efc0de317f3fc42eeef
Raj Kanchan <iamraajkanchan@gmail.com>
2024-01-18 13:19:35 +0530

    LightSensorActivity.kt, LightFragmentHolder.kt and AndroidManifest.xml is modified.

--- app/src/main/java/com/family/lucky/lightSensor/LightFragmentHolder.kt
+++ app/src/main/java/com/family/lucky/lightSensor/LightFragmentHolder.kt
@@ -2,21 +2,53 @@ package com.family.lucky.lightSensor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.family.lucky.databinding.FragmentBasicLightBinding
+import com.family.lucky.databinding.ActivityLightFragmentHolderBinding
 import com.family.lucky.lightSensor.fragments.AdjustScreenFragment
 import com.family.lucky.lightSensor.fragments.BasicLightFragment
+import com.family.lucky.lightSensor.fragments.UsingLightFragment
 
 class LightFragmentHolder : AppCompatActivity() {
-    private lateinit var binding: FragmentBasicLightBinding
+    private lateinit var binding: ActivityLightFragmentHolderBinding
+    private var fragmentExtra = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = FragmentBasicLightBinding.inflate(layoutInflater)
+        binding = ActivityLightFragmentHolderBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        if (intent.hasExtra(FRAGMENT_EXTRA)) {
+            fragmentExtra = intent.getStringExtra(FRAGMENT_EXTRA) ?: BASIC_LIGHT
+        }
         initViews()
     }
 
     private fun initViews() {
         val basicLightFragment = BasicLightFragment.newInstance()
         val adjustScreenFragment = AdjustScreenFragment.newInstance()
+        val usingLightFragment = UsingLightFragment.newInstance()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        when (fragmentExtra) {
+            BASIC_LIGHT -> {
+                fragmentTransaction.replace(binding.fcvFragment.id, basicLightFragment)
+            }
+
+            ADJUST_SCREEN -> {
+                fragmentTransaction.replace(binding.fcvFragment.id, adjustScreenFragment)
+            }
+
+            USING_LIGHT -> {
+                fragmentTransaction.replace(binding.fcvFragment.id, usingLightFragment)
+            }
+            else -> {
+                return
+            }
+        }
+        fragmentTransaction.disallowAddToBackStack()
+        fragmentTransaction.commit()
+    }
+
+    companion object {
+        const val FRAGMENT_EXTRA = "fragment"
+        const val BASIC_LIGHT = "basic light fragment"
+        const val ADJUST_SCREEN = "adjust screen fragment"
+        const val USING_LIGHT = "using light fragment"
     }
 }
--- app/src/main/java/com/family/lucky/lightSensor/LightSensorActivity.kt
+++ app/src/main/java/com/family/lucky/lightSensor/LightSensorActivity.kt
@@ -1,5 +1,6 @@
 package com.family.lucky.lightSensor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.family.lucky.databinding.ActivityLightSensorBinding
@@ -10,5 +11,27 @@ class LightSensorActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLightSensorBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.btnBasicLight.setOnClickListener {
+            Intent(this@LightSensorActivity, LightFragmentHolder::class.java).apply {
+                putExtra(LightFragmentHolder.FRAGMENT_EXTRA, LightFragmentHolder.BASIC_LIGHT)
+                startActivity(this)
+            }
+        }
+        binding.btnAdjustScreen.setOnClickListener {
+            Intent(this@LightSensorActivity, LightFragmentHolder::class.java).apply {
+                putExtra(LightFragmentHolder.FRAGMENT_EXTRA, LightFragmentHolder.ADJUST_SCREEN)
+                startActivity(this)
+            }
+        }
+        binding.btnUsingLight.setOnClickListener {
+            Intent(this@LightSensorActivity, LightFragmentHolder::class.java).apply {
+                putExtra(LightFragmentHolder.FRAGMENT_EXTRA, LightFragmentHolder.USING_LIGHT)
+                startActivity(this)
+            }
+        }
     }
 }
